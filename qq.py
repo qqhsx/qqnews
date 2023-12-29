995284602770716b0ee11525ed6907fea49e2b87
qqhsx <56535272+qqhsx@users.noreply.github.com>
2023-12-29 10:54:52 +0800

    Update qq.py

--- qq.py
+++ qq.py
@@ -11,10 +11,8 @@ from concurrent.futures import ThreadPoolExecutor
 import hashlib
 import subprocess
 
-logging.basicConfig(filename='news_crawler.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def sanitize_filename(filename):
-    return "".join([c for c in filename if c.isalpha() or c.isdigit() or c in (' ','.','_')]).rstrip('. \t\n')
+logging.basicConfig(filename='news_crawler.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def push_image_to_repo(file_path, repo_name, repo_owner, branch='main', commit_message='Add new image'):
     access_token = os.getenv('IMAGE_REPO_TOKEN')  # 确保在GitHub Actions secrets中设置了IMAGE_REPO_TOKEN
@@ -70,19 +68,31 @@ def download_image(url, output_dir, filename, retries=3, repo_name='qqnews_image
     return None
 
 def patch_fix_image_links(text, output_dir, title):
+    # 正则表达式，用于匹配Markdown中的图像链接
     img_pattern = r'!\[[^\]]*\]\([^)]*\)'
+
+    # 查找所有图片链接
     img_links = re.findall(img_pattern, text)
+
+    # 遍历图片链接并修复换行符
     for img_link in img_links:
         fixed_img_link = img_link.replace('\n', '')
         text = text.replace(img_link, fixed_img_link)
+
+        # 下载图片到指定目录
         img_url = re.search(r'\((.*?)\)', fixed_img_link).group(1)
         filename = f"{hashlib.md5(img_url.encode()).hexdigest()}.jpg"
-        download_image(img_url, output_dir, filename, retries=3)
+        download_image(img_url, output_dir, filename)
+
         if filename:
+            # 用相对路径替换图像链接
             relative_path = f"./{title}/{filename}"
             text = text.replace(fixed_img_link, f"![{filename}]({relative_path})")
+
+    # 修复链接和文本显示在同行上的问题
     text = re.sub(r'(!\[[^\]]*\]\([^)]*\))(\n{1,2})?', r'\1\n\n', text)
     text = re.sub(r'(!\[[^\]]*\]\([^)]*\))(.)', r'\1\n\n\2', text)
+
     return text
 
 def process_article(title, url):
@@ -95,19 +105,24 @@ def process_article(title, url):
         if len(ss.split()) <= 3:
             logging.info(f"INVALID\n{title}\n{url}\n")
             return
+
         year = time.strftime('%Y')
         month = time.strftime('%m')
         day = time.strftime('%d')
         output_dir = os.path.join(sys.path[0], year, month, day)
-        sanitized_title = sanitize_filename(title)
-        img_dir = os.path.join(output_dir, sanitized_title)
+        img_dir = os.path.join(output_dir, title)
+
         if not os.path.exists(img_dir):
             os.makedirs(img_dir)
-        ss = patch_fix_image_links(ss, img_dir, sanitized_title)
-        output_file = os.path.join(output_dir, f"{sanitized_title}.md")
+
+        # Fix image links and newline characters in Markdown file
+        ss = patch_fix_image_links(ss, img_dir, title)
+
+        output_file = os.path.join(output_dir, f"{title}.md")
         if os.path.exists(output_file):
             logging.info(f"EXIST\n{title}\n{url}\n")
             return
+
         with open(output_file, "w", encoding="utf-8") as x:
             x.write(ss)
         logging.info(f"SUCCESS\n{title}\n{url}\n")
@@ -128,9 +143,12 @@ def main():
             logging.info(f"EXPIRED\n{tmptitle}\n{tmpurl}\n")
             continue
         datalist.append(tuple([tmptitle, tmpurl]))
+
+    # Use thread pool to handle article download and processing
     with ThreadPoolExecutor(max_workers=5) as executor:
         for item in datalist:
             executor.submit(process_article, item[0], item[1])
+
     print(f"Total time taken: {time.time() - starttime} seconds")
 
 if __name__ == '__main__':
